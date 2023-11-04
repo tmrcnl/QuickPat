8f00c5d90bf2e84a8ecf2d82263d4850573a27d9
Stephen He <stephen.he@uber.com>
2023-11-04 18:38:55 -0400

    Parse out the publ number, abstract, and claims from ~100 patents

---
+++ read.py
@@ -0,0 +1,44 @@
+import bs4
+import parser
+
+with open("ipa200102.xml", "r") as f:
+    xml = f.read()
+
+split_xml = xml.split('<?xml version="1.0" encoding="UTF-8"?>')
+split_xml = split_xml[:100] # for testing
+
+class Patent:
+    def __init__(self, publ_number, abstract, claims):
+        self.publ_number = publ_number
+        self.abstract = abstract
+        self.claims = claims
+
+    def __str__(self):
+        return f"Publication Number: {self.publ_number}\nAbstract: {self.abstract}\nClaims: {self.claims}"
+
+patents = []
+for single_xml in split_xml:
+    if len(single_xml) == 0:
+        # not an xml doc
+        continue
+    soup = bs4.BeautifulSoup(single_xml, "xml")
+
+    if not parser.isPatentApplication(soup):
+        continue
+
+    publ_number = parser.getPublicationNumber(soup)
+    # print("publ_number", publ_number)
+
+    abstract = parser.getAbstract(soup)
+    # print("abstract", abstract)
+
+    claim_data = parser.getClaimData(soup)
+    # print("claim_data", claim_data)
+
+    patents.append(Patent(publ_number, abstract, claim_data))
+
+for patent in patents[:5]:
+    print("--------------------------------------------------")
+    print(patent)
+
+print(len(patents))
